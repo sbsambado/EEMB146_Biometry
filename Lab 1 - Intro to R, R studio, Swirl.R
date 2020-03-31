16ce2104cf3c2b7809b544a1201d880f79df77b8
sbsambado <sbsambado@ucsb.edu>
2020-03-31 10:51:56 -0700

    lab 1 for EEMB 146

---
+++ Lab 1 - Intro to R, R studio, Swirl.R
@@ -0,0 +1,38 @@
+# EEMB 146 - Biometry 
+# Discussion: Tuesday at 10 AM with TA Sam
+
+## Class announcements
+  # 1. Join Piazza
+  # 2. Computer labs are 30% of your total grade
+  # 3. Students can attend any lab they want!
+  # 4. Watch video tutorials before coming to lab
+  # 5. Lab attendence is not required, more like OH
+  # 6. Hw due every week on Wednesday (except this week)
+        # 8 HW assignments (.html or .pdf, not .rmd)
+        # Graded on completeness and correctness
+  # 7. Available online via piazza or email for questions
+
+
+## Assignment and homework for Lab 1
+  # follow https://swirlstats.com/students.html
+      # Step 1: Get R
+      # Step 2: Get R Studio
+      # Step 3: Install swirl
+install.packages('swirl')
+      # Step 4: Start swirl
+        library(swirl)
+        swirl()
+      # Step 5: Install an interactive course
+        # 1:1: R programming
+      # Step 6: Have fun!
+        
+    ## Homework is to go through basics of programming in R,
+        # 1, 2, 3, 4, 12
+        
+  # Welcome to try more or explore other R courses!
+        
+        ## 3 common syntax errors
+        # 1. mispelling install.packages /=/ enstall.package
+        # 2. capitilizing install.packages /=/ Install.packages
+        # 3. punctuation install.packages /=/ install,packages
+        
